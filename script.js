eb09ca0c4080d57595bec2b81b849289a1bb0b7d
Helen Liutongco <liutongcoh@gmail.com>
2019-02-09 19:33:17 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function success(pos) {
+  const crd = pos.coords;
+  const div = document.getElementById('text');
+  const innerDiv = document.createElement('div')
+  innerDiv.innerHTML = `
+  <p>Your current position is:</p>
+  <p>Latitude : ${crd.latitude}</p>
+  <p>Longitude: ${crd.longitude}</p>
+  <p>More or less ${crd.accuracy} meters.</p>
+  `
+  div.appendChild(innerDiv)
+}
+
+navigator.geolocation.watchPosition(success)

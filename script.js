50fc1feebd4c9d8bfa44e58b92fcf075509d8aed
Helen Liutongco <liutongcoh@gmail.com>
2019-02-09 19:54:27 -0500

    bah

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+let currentLat = 0
+let currentLong = 0
+
 function success(pos) {
   const crd = pos.coords;
   const div = document.getElementById('text');
   const innerDiv = document.createElement('div')
-
-
-
-  innerDiv.innerHTML = `
-  <p>Your current position is:</p>
-  <p>Latitude : ${crd.latitude}</p>
-  <p>Longitude: ${crd.longitude}</p>
-  <p>More or less ${crd.accuracy} meters.</p>
+  const latCondition = Math.round(crd.latitude) === Math.round(currentLat)
+  const longCondition = Math.round(crd.longitude) === Math.round(currentLong)
+  if(!currentLat || !currentLong || !latCondition && !longCondition){
+    innerDiv.innerHTML = `
+    <p>Your current position is:</p>
+    <p>Latitude : ${crd.latitude}</p>
+    <p>Longitude: ${crd.longitude}</p>
+    <p>More or less ${crd.accuracy} meters.</p>
+}
   `
   div.appendChild(innerDiv)
 }
